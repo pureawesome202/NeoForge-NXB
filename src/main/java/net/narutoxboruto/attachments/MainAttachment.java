a69a05ecdc31ee45759941145ea315e0dc101e48
Turgyn <130315908+Turgyn123@users.noreply.github.com>
2025-10-20 17:13:19 +0200

    small fix

--- src/main/java/net/narutoxboruto/attachments/MainAttachment.java
+++ src/main/java/net/narutoxboruto/attachments/MainAttachment.java
@@ -21,7 +21,7 @@ public class MainAttachment {
     public static final Supplier<AttachmentType<Affiliation>> AFFILIATION = ATTACHMENT_TYPES.register("affiliaton", () -> AttachmentType.<Affiliation>builder(() -> new Affiliation()).serialize(Affiliation.CODEC).copyOnDeath().build());
     public static final Supplier<AttachmentType<ReleaseList>> RELEASE_LIST = ATTACHMENT_TYPES.register("release_list", () -> AttachmentType.<ReleaseList>builder(() -> new ReleaseList()).serialize(ReleaseList.CODEC).copyOnDeath().build());
     public static final Supplier<AttachmentType<Genjutsu>> GENJUTSU = ATTACHMENT_TYPES.register("genjutsu", () -> AttachmentType.<Genjutsu>builder(() -> new Genjutsu()).serialize(Genjutsu.CODEC).copyOnDeath().build());
-    public static final Supplier<AttachmentType<Kenjutsu>> KENJUTSU = ATTACHMENT_TYPES.register("Kenjutsu", () -> AttachmentType.<Kenjutsu>builder(() -> new Kenjutsu()).serialize(Kenjutsu.CODEC).copyOnDeath().build());
+    public static final Supplier<AttachmentType<Kenjutsu>> KENJUTSU = ATTACHMENT_TYPES.register("kenjutsu", () -> AttachmentType.<Kenjutsu>builder(() -> new Kenjutsu()).serialize(Kenjutsu.CODEC).copyOnDeath().build());
     public static final Supplier<AttachmentType<Kinjutsu>> KINJUTSU = ATTACHMENT_TYPES.register("kinjutsu", () -> AttachmentType.<Kinjutsu>builder(() -> new Kinjutsu()).serialize(Kinjutsu.CODEC).copyOnDeath().build());
     public static final Supplier<AttachmentType<Medical>> MEDICAL = ATTACHMENT_TYPES.register("medical", () -> AttachmentType.<Medical>builder(() -> new Medical()).serialize(Medical .CODEC).copyOnDeath().build());
     public static final Supplier<AttachmentType<Ninjutsu>> NINJUTSU = ATTACHMENT_TYPES.register("ninjutsu", () -> AttachmentType.<Ninjutsu>builder(() -> new Ninjutsu()).serialize(Ninjutsu.CODEC).copyOnDeath().build());
@@ -30,13 +30,13 @@ public class MainAttachment {
     public static final Supplier<AttachmentType<Speed>> SPEED = ATTACHMENT_TYPES.register("speed", () -> AttachmentType.<Speed>builder(() -> new Speed()).serialize(Speed.CODEC).copyOnDeath().build());
     public static final Supplier<AttachmentType<Summoning>> SUMMONING = ATTACHMENT_TYPES.register("summoning", () -> AttachmentType.<Summoning>builder(() -> new Summoning()).serialize(Summoning.CODEC).copyOnDeath().build());
     public static final Supplier<AttachmentType<Taijutsu>> TAIJUTSU = ATTACHMENT_TYPES.register("taijutsu", () -> AttachmentType.<Taijutsu>builder(() -> new Taijutsu()).serialize(Taijutsu.CODEC).copyOnDeath().build());
-    public static final Supplier<AttachmentType<EarthList>> EARTHLIST = ATTACHMENT_TYPES.register("earthList", () -> AttachmentType.<EarthList>builder(() -> new EarthList()).serialize(EarthList.CODEC).copyOnDeath().build());
-    public static final Supplier<AttachmentType<FireList>> FIRELIST = ATTACHMENT_TYPES.register("fireList", () -> AttachmentType.<FireList>builder(() -> new FireList()).serialize(FireList.CODEC).copyOnDeath().build());
-    public static final Supplier<AttachmentType<LightningList>> LIGHTINGLIST = ATTACHMENT_TYPES.register("lightningList", () -> AttachmentType.<LightningList>builder(() -> new LightningList()).serialize(LightningList.CODEC).copyOnDeath().build());
-    public static final Supplier<AttachmentType<WaterList>> WATERLIST = ATTACHMENT_TYPES.register("waterList", () -> AttachmentType.<WaterList>builder(() -> new WaterList()).serialize(WaterList.CODEC).copyOnDeath().build());
-    public static final Supplier<AttachmentType<WindList>> WINDLIST = ATTACHMENT_TYPES.register("windList", () -> AttachmentType.<WindList>builder(() -> new WindList()).serialize(WindList.CODEC).copyOnDeath().build());
-    public static final Supplier<AttachmentType<YangList>> YANGLIST = ATTACHMENT_TYPES.register("yangList", () -> AttachmentType.<YangList>builder(() -> new YangList()).serialize(YangList.CODEC).copyOnDeath().build());
-    public static final Supplier<AttachmentType<YingList>> YINGLIST = ATTACHMENT_TYPES.register("yingList", () -> AttachmentType.<YingList>builder(() -> new YingList()).serialize(YingList.CODEC).copyOnDeath().build());
+    public static final Supplier<AttachmentType<EarthList>> EARTHLIST = ATTACHMENT_TYPES.register("earth", () -> AttachmentType.<EarthList>builder(() -> new EarthList()).serialize(EarthList.CODEC).copyOnDeath().build());
+    public static final Supplier<AttachmentType<FireList>> FIRELIST = ATTACHMENT_TYPES.register("firet", () -> AttachmentType.<FireList>builder(() -> new FireList()).serialize(FireList.CODEC).copyOnDeath().build());
+    public static final Supplier<AttachmentType<LightningList>> LIGHTINGLIST = ATTACHMENT_TYPES.register("lightning", () -> AttachmentType.<LightningList>builder(() -> new LightningList()).serialize(LightningList.CODEC).copyOnDeath().build());
+    public static final Supplier<AttachmentType<WaterList>> WATERLIST = ATTACHMENT_TYPES.register("water", () -> AttachmentType.<WaterList>builder(() -> new WaterList()).serialize(WaterList.CODEC).copyOnDeath().build());
+    public static final Supplier<AttachmentType<WindList>> WINDLIST = ATTACHMENT_TYPES.register("wind", () -> AttachmentType.<WindList>builder(() -> new WindList()).serialize(WindList.CODEC).copyOnDeath().build());
+    public static final Supplier<AttachmentType<YangList>> YANGLIST = ATTACHMENT_TYPES.register("yang", () -> AttachmentType.<YangList>builder(() -> new YangList()).serialize(YangList.CODEC).copyOnDeath().build());
+    public static final Supplier<AttachmentType<YingList>> YINGLIST = ATTACHMENT_TYPES.register("ying", () -> AttachmentType.<YingList>builder(() -> new YingList()).serialize(YingList.CODEC).copyOnDeath().build());
 
     public static void register(IEventBus modEventBus) {
         ATTACHMENT_TYPES.register(modEventBus);
